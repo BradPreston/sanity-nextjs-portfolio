30cd7aa13617bee43356cff048f704f31a57c980
bradpreston <bap5393@gmail.com>
2025-12-18 21:54:13 -0500

    Update font weight on button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 
 export default function Button({ label, url, variant, newTab }: ButtonProps) {
   return (
-    <Link href={url} target={newTab ? "_blank" : "_self"} className={cn("inline-flex items-center justify-center py-1 px-5 transition-colors border-2 border-primary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 rounded-full hover:bg-primary-hover hover:text-primary", variant === "primary" && "bg-primary text-dark", variant === "outline" && "bg-dark text-primary")}>
+    <Link href={url} target={newTab ? "_blank" : "_self"} className={cn("inline-flex font-medium items-center justify-center py-1 px-5 transition-colors border-2 border-primary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 rounded-full hover:bg-primary-hover hover:text-primary", variant === "primary" && "bg-primary text-dark", variant === "outline" && "bg-dark text-primary")}>
       {label}
     </Link>
   )
